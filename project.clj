d70783f231ec7c77488d50890435d3655aa2ff58
Micha Niskin <asdf@ubergibson.com>
2012-10-03 10:35:15 -0400

    workin, added repl; fixed issue with ring

--- project.clj
+++ project.clj
@@ -6,11 +6,13 @@
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj"]
   :plugins      [[lein-hlisp "0.1.0-SNAPSHOT"]
+                 [lein-cljsbuild "0.2.4"]
                  [lein-ring "0.7.1"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.0.4"]]
   :eval-in      :leiningen
   :ring         {:handler example.routes/app}
+  :cljsbuild    {:builds []}
   :hlisp        {:html-src    "src/html"
                  :cljs-src    "src/cljs"
                  :html-out    "resources/public"
